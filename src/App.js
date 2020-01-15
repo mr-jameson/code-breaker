5ebd1d92138e51e41d1cc7f6d256a90e672ec27f
waynezdev <cas021919@coderacademy.edu.au>
2020-01-15 11:57:56 +1100

    created timer component

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
+
+class App extends Component {
+    render () {
+        return (
+            <div>
+                <h1>CODE BREAKER</h1>
+                timer goes here
+            </div>
+        )
+    }
+    
+
+    
+
+}
+
+
+
+export default App;
