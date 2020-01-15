c34d5362af8c706e6820b87abe0a82a98f2f31e9
mr-jameson <elliot.jameson4@gmail.com>
2020-01-15 12:49:39 +1100

    Commit before pulling master

--- src/components/TimerForm.js
+++ src/components/TimerForm.js
@@ -4,11 +4,11 @@ class TimerForm extends Component {
     render() {
         return(
         <form>
-        <label>
-            Time:
-            <input type="text" name="time" />
-        </label>
-        <input type="submit" value="Submit" />
+            <label>
+                Time:
+                <input type="text" name="time" />
+            </label>
+            <input type="submit" value="Submit" />
         </form>
         );
     }
