94637149e797404e7db45334d52634295ef343b3
mr-jameson <elliot.jameson4@gmail.com>
2020-01-15 12:31:12 +1100

    added component TimerForm

--- src/components/TimerForm.js
+++ src/components/TimerForm.js
@@ -1,1 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+class TimerForm extends Component {
+    render() {
+        return(
+        <form>
+        <label>
+            Time:
+            <input type="text" name="time" />
+        </label>
+        <input type="submit" value="Submit" />
+        </form>
+        );
+    }
+}
+
+export default TimerForm;
