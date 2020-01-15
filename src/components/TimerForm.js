b316b2e79ff76341a72aa9ddf69a7c706042cec2
mr-jameson <elliot.jameson4@gmail.com>
2020-01-15 14:27:53 +1100

    updated TimerForm with event listeners

--- src/components/TimerForm.js
+++ src/components/TimerForm.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 
 class TimerForm extends Component {
+    constructor(props) {
+        this.state = { time: ''}
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.time })
+    }
+
+    handleSubmit(event) {
+        alert(`Timer is set for ${this.state.time} seconds`);
+        event.preventDefault();
+    }
+
     render() {
         return(
         <form>
             <label>
                 Time:
-                <input type="text" name="time" />
+                <input type="text" time={this.state.time} onChange={this.handleChange} />
             </label>
             <input type="submit" value="Submit" />
         </form>
